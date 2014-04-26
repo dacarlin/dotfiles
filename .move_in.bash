246017ed8537ddcb1778764b080289a0f52adfbf
Alex Carlin <dacarlin@ucdavis.edu>
2014-04-26 16:37:42 -0700

    Created move-in script

---
+++ .move_in.bash
@@ -0,0 +1,14 @@
+# Set up a new environment with all the necessities 
+
+
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+brew doctor 
+# install homebrew 
+
+# symlink config files
+
+brew install git
+brew install python3
+# install git, python
+
+# install R?
