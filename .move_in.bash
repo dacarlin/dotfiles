a24a3282d9df8a107beb7ac7ae29da0338248ba3
Alex Carlin <carlin@ucdavis.edu>
2014-04-27 16:49:37 -0700

    Commented out .move_in.bash in case someone accidentally runs it

--- .move_in.bash
+++ .move_in.bash
@@ -1,14 +1,8 @@
 # Set up a new environment with all the necessities 
-
-
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-brew doctor 
-# install homebrew 
-
-# symlink config files
-
-brew install git
-brew install python3
-# install git, python
-
+# ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+# brew doctor 
+# brew install git
+# brew install python3
+# git clone git@github.com:dacarlin/dotfiles.git dotfiles
+# bash link.bash
 # install R?
