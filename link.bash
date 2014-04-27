60eb6f5aae291a2890111c9b2fa68d8db85860f2
Alex Carlin <carlin@ucdavis.edu>
2014-04-27 16:49:19 -0700

    Wrote link.bash to delete existing config files and symlink in new ones from this repo

--- link.bash
+++ link.bash
@@ -1,1 +1,12 @@
-link ./.vimrc ~/.vimrc
+mv ~/.bashrc ~./Trash
+mv ~/.bash_profile ~/.Trash
+mv ~/.vimrc ~/.Trash
+mv ~/.pythonrc ~/.Trash
+mv ~/.pymolrc ~/.Trash
+mv ~/.zshrc ~/.Trash
+ln -s .bashrc ~/.bashrc
+ln -s .bash_profile ~/.bash_profile
+ln -s .vimrc ~/.vimrc
+ln -s .pythonrc ~/.pythonrc
+ln -s .pymolrc ~/.pymolrc
+ln -s .zshrc ~/.zshrc
