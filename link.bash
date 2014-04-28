514a16837a5c185811aaa1b8978e0d6c8be836f4
Alex Carlin <carlin@ucdavis.edu>
2014-04-27 17:02:02 -0700

    Saved

--- link.bash
+++ link.bash
@@ -1,12 +1,13 @@
-mv ~/.bashrc ~./Trash
+#!/bin/bash
+mv ~/.bashrc ~/.Trash
 mv ~/.bash_profile ~/.Trash
 mv ~/.vimrc ~/.Trash
 mv ~/.pythonrc ~/.Trash
 mv ~/.pymolrc ~/.Trash
 mv ~/.zshrc ~/.Trash
-ln -s .bashrc ~/.bashrc
-ln -s .bash_profile ~/.bash_profile
-ln -s .vimrc ~/.vimrc
-ln -s .pythonrc ~/.pythonrc
-ln -s .pymolrc ~/.pymolrc
-ln -s .zshrc ~/.zshrc
+ln -s ./.bashrc ~/.bashrc
+ln -s ./.bash_profile ~/.bash_profile
+ln -s ./.vimrc ~/.vimrc
+ln -s ./.pythonrc ~/.pythonrc
+ln -s ./.pymolrc ~/.pymolrc
+ln -s ./.zshrc ~/.zshrc
