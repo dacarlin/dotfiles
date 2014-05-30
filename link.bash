22306ed807e59999286d01b990fa8066c92fcb9c
Alex Carlin <dacarlin@ucdavis.edu>
2014-05-29 18:11:16 -0700

    Update link.bash

--- link.bash
+++ link.bash
@@ -5,9 +5,11 @@ rm ~/.vimrc
 rm ~/.pythonrc
 rm ~/.pymolrc 
 rm ~/.zshrc
+rm ~/.gitignore
 ln -s /Users/alex/Documents/dotfiles/.bashrc ~/.bashrc
 ln -s /Users/alex/Documents/dotfiles/.bash_profile ~/.bash_profile
 ln -s /Users/alex/Documents/dotfiles/.vimrc ~/.vimrc
 ln -s /Users/alex/Documents/dotfiles/.pythonrc ~/.pythonrc
 ln -s /Users/alex/Documents/dotfiles/.pymolrc ~/.pymolrc
 ln -s /Users/alex/Documents/dotfiles/.zshrc ~/.zshrc
+ln -s /Users/alex/Documents/dotfiles/.gitignore ~/.gitignore
