f81f5335d2d6558ff972039a533351dbfd9f2141
Alex Carlin <dacarlin@users.noreply.github.com>
2017-02-16 13:26:21 -0800

    Delete link.bash

--- link.bash
+++
@@ -1,7 +0,0 @@
-mkdir orig_dotfiles
-for dotfile in .Rprofile .bashrc .bash_profile \
-               .vimrc .pymolrc .gitignore .gitconfig
-do
-  mv ~/$dotfile orig-dotfiles/$dotfile-orig
-  ln -s $dotfile ~/$dotfile
-done
